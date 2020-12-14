0111e55c6810fcd7fdaf191e850ef9760015fcc7
Dawid Wegner <dawid.wegner@gmail.com>
2020-12-14 21:52:53 +0100

    Update training scripts

--- scripts/run_transe_wn18rr.sh
+++ scripts/run_transe_wn18rr.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-python3 ../src/train_model.py --gin_configs ../configs/wn18rr_transe_training_config.gin
+python3 ../src/train_model.py \
+    --gin_configs ../configs/wn18rr_transe_training_config.gin \
+    --gin_bindings "create_learning_rate_schedule.initial_learning_rate = 1e-2" \
+    "ExperimentConfig.steps_per_evaluation = 10" \
+    "Dataset.batch_size = 3000" \
+    "ExperimentConfig.experiment_name = 'transe_wn18rr_baseline'"
+
--- scripts/run_transe_wn18rr_batch256.sh
+++ scripts/run_transe_wn18rr_batch256.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-python3 ../src/train_model.py --gin_configs ../configs/wn18rr_transe_training_config.gin --gin_bindings "create_learning_rate_schedule.initial_learning_rate = 1e-4" "create_learning_rate_schedule.decay_steps = 5000" "create_learning_rate_schedule.decay_rate = 0.5" "ExperimentConfig.steps_per_evaluation = 50" "Dataset.batch_size = 256" "ExperimentConfig.experiment_name = 'transe_wn18rr_batch256'"
+python3 ../src/train_model.py \
+    --gin_configs ../configs/wn18rr_transe_training_config.gin \
+    --gin_bindings "create_learning_rate_schedule.initial_learning_rate = 1e-3" \
+    "create_learning_rate_schedule.decay_steps = 10_000" \
+    "create_learning_rate_schedule.decay_rate = 0.5" \
+    "ExperimentConfig.steps_per_evaluation = 100" \
+    "Dataset.batch_size = 256" \
+    "ExperimentConfig.experiment_name = 'transe_wn18rr_batch256'"
