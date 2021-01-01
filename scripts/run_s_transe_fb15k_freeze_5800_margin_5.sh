946f421e643954df2a8480dd52e2e30bb72284a8
Dawid Wegner <dawid.wegner@gmail.com>
2021-01-01 16:08:08 +0100

    Add configs for running STranse model

---
+++ scripts/run_s_transe_fb15k_freeze_5800_margin_5.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python3 ../src/train_model.py \
+    --gin_configs ../configs/fb15k_s_transe_training_config.gin \
+    --gin_bindings "LossObject.norm_metric_margin = 5.0" \
+    "s_transe/EmbeddingsTransformConfig.trainable_transformations_min_iteration = 5800" \
+    "create_learning_rate_schedule.initial_learning_rate = 3e-3" \
+    "create_learning_rate_schedule.decay_steps = 5800" \
+    "ExperimentConfig.experiment_name = 's_transe_fb15k_freeze_5800_margin_5'"
---
+++ scripts/s_transe_wn18rr_freeze_5800_margin_6_lr_decay_0_03.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python3 ../src/train_model.py \
+    --gin_configs ../configs/wn18rr_s_transe_training_config.gin \
+    --gin_bindings "LossObject.norm_metric_margin = 6.0" \
+    "s_transe/EmbeddingsTransformConfig.trainable_transformations_min_iteration = 5800" \
+    "create_learning_rate_schedule.initial_learning_rate = 1e-2" \
+    "create_learning_rate_schedule.decay_steps = 5800" \
+    "create_learning_rate_schedule.decay_rate = 0.03" \
+    "ExperimentConfig.experiment_name = 's_transe_wn18rr_freeze_5800_margin_6'"
