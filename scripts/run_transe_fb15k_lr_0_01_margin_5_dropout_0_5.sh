5bc16a3aff13c2314e551fd9eb5b133afffd04f9
Dawid Wegner <dawid.wegner@gmail.com>
2021-02-01 14:14:07 +0100

    Update scripts for running Transe models

---
+++ scripts/run_transe_fb15k_lr_0_01_margin_5_dropout_0_5.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python3 ../src/train_model.py \
+    --gin_configs ../configs/fb15k_transe_training_config.gin \
+    --gin_bindings "create_learning_rate_schedule.initial_learning_rate = 1e-2" \
+    "ExperimentConfig.steps_per_evaluation = 50" \
+    "Dataset.batch_size = 10_000" \
+    "LossObject.norm_metric_margin = 5.0" \
+    "s_transe/ModelConfig.dropout_rate = 0.5" \
+    "ExperimentConfig.experiment_name = 'transe_fb15k_lr_0_01_margin_5_dropout_0_5'"
---
+++ scripts/run_transe_wn18rr_softplus_lr_0_001.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python3 ../src/train_model.py \
+    --gin_configs ../configs/wn18rr_transe_training_config.gin \
+    --gin_bindings "create_learning_rate_schedule.initial_learning_rate = 1e-3" \
+    "ExperimentConfig.steps_per_evaluation = 50" \
+    "Dataset.batch_size = 3000" \
+    "LossObject.optimized_metric = %OptimizedMetric.SOFTPLUS" \
+    "transe/ModelConfig.include_reduce_dim_layer = True" \
+    "ExperimentConfig.experiment_name = 'transe_wn18rr_softplus_lr_0_001'"
