218f42afec38a2fe0dc2d8074d9d879e58b50761
Dawid Wegner <dawid.wegner@gmail.com>
2021-07-11 18:58:27 +0200

    Add ConvE config for FB15K dataset

--- search_scripts/run_conve_wn18rr.sh
+++ search_scripts/run_conve_wn18rr.sh
@@ -3,4 +3,4 @@
 python3 ../src/run_parameter_search.py \
     --gin_configs ../configs/wn18rr_conve_training_config.gin \
     --gin_bindings "ExperimentConfig.experiment_name = 'conve_parameter_search_wn18rr'" \
-    --search_config ../search_configs/conve_wn18rr.json
+    --search_config ../search_configs/conve.json
