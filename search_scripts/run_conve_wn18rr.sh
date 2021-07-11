9c53bdd4edb69cd6c4be9d5a1f422203d172dece
Dawid Wegner <dawid.wegner@gmail.com>
2021-07-11 14:00:37 +0200

    Update configs for ConvE model

--- search_scripts/run_conve_wn18rr.sh
+++ search_scripts/run_conve_wn18rr.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 python3 ../src/run_parameter_search.py \
-    --gin_configs ../configs/wn18rr_transe_training_config.gin \
-    --gin_bindings "ExperimentConfig.experiment_name = 'transe_parameter_search_wn18rr'" \
-    --search_config ../search_configs/transe_wn18rr.json
-
+    --gin_configs ../configs/wn18rr_conve_training_config.gin \
+    --gin_bindings "ExperimentConfig.experiment_name = 'conve_parameter_search_wn18rr'" \
+    --search_config ../search_configs/conve_wn18rr.json
