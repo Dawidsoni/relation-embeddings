63d49ab47c50b3b1204155d77008e07b28635623
Dawid Wegner <dawid.wegner@gmail.com>
2021-07-17 16:46:04 +0200

    Include model's loss in stopping criteria

--- src/optimization/model_evaluators.py
+++ src/optimization/model_evaluators.py
@@ -142,6 +142,7 @@ class SamplingModelEvaluator(ModelEvaluator):
         tf.summary.scalar(name="losses/positive_samples_loss", data=positive_samples_loss, step=step)
         regularization_loss = self.loss_object.get_regularization_loss(self.model)
         tf.summary.scalar(name="losses/regularization_loss", data=regularization_loss, step=step)
+        return positive_samples_loss
 
     def _compute_and_report_model_outputs(self, positive_samples, step):
         positive_outputs = self.model(positive_samples, training=False)
@@ -158,10 +159,10 @@ class SamplingModelEvaluator(ModelEvaluator):
         positive_samples, unused_negative_samples = next(self.iterator_of_samples)
         with self.summary_writer.as_default():
             metrics = self._compute_and_report_metrics(positive_samples, step)
-            self._compute_and_report_losses(positive_samples, step)
+            loss_value = self._compute_and_report_losses(positive_samples, step)
             self._compute_and_report_model_outputs(positive_samples, step)
             self._maybe_report_learning_rate(step)
-        return _extract_lower_is_better_metrics(metrics["metrics_averaged"])
+        return _extract_lower_is_better_metrics(metrics["metrics_averaged"]) + (loss_value, )
 
     def log_metrics(self, logger):
         positive_samples_iterator = self.dataset.samples.map(
@@ -220,6 +221,7 @@ class SoftmaxModelEvaluator(ModelEvaluator):
         tf.summary.scalar(name="losses/samples_loss", data=loss_value, step=step)
         regularization_loss = self.loss_object.get_regularization_loss(self.model)
         tf.summary.scalar(name="losses/regularization_loss", data=regularization_loss, step=step)
+        return loss_value
 
     def build_model(self):
         self.model(next(self.iterator_of_samples), training=False)
@@ -228,9 +230,9 @@ class SoftmaxModelEvaluator(ModelEvaluator):
         samples = next(self.iterator_of_samples)
         with self.summary_writer.as_default():
             metrics = self._compute_and_report_metrics(samples, step)
-            self._compute_and_report_losses(samples, step)
+            loss_value = self._compute_and_report_losses(samples, step)
             self._maybe_report_learning_rate(step)
-        return _extract_lower_is_better_metrics(metrics["metrics_averaged"])
+        return _extract_lower_is_better_metrics(metrics["metrics_averaged"]) + (loss_value, )
 
     def log_metrics(self, logger):
         test_samples_count = 2 * len(self.dataset.graph_edges)
